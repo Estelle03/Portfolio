4694d49f32fcabed269b56e6c5a435fc581432ba
Estelle03 <98829413+Estelle03@users.noreply.github.com>
2023-01-09 14:33:24 +0100

    Delete DesktopJs.js

--- DesktopJs.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('.flip-box').addEventListener('click', function (clic) {
-    console.log('click');
-    document.querySelector('.mmi').remove({ animate});
-    setTimeout(function () {
-        document.querySelector('.mmi').setAttribute('class', 'mmi');
-    }, 200);
-});
